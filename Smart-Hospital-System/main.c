b60615952dbb27e2b7aa2e886b22d57c95572c91
Amiruthennakoon <amirut2005@gmail.com>
2026-09-05 17:43:57 +0530

    Add global parallel arrays and 2D bed occupancy matrix

--- Smart-Hospital-System/main.c
+++ Smart-Hospital-System/main.c
@@ -17,3 +17,15 @@ const int WARD_IDS[NUM_WARDS] = {1, 2, 3, 4};
 const char WARD_NAMES[NUM_WARDS][30] = {"General Ward", "Paediatric Ward", "Surgical Ward", "ICU (Intensive Care Unit)"};
 const double WARD_DAILY_RATES[NUM_WARDS] = {3000.00, 6000.00, 12000.00, 25000.00};
 const int WARD_BED_CAPACITIES[NUM_WARDS] = {20, 10, 10, 5};
+
+int bedOccupancy[NUM_WARDS][MAX_BEDS_PER_WARD] = {0};
+int specialtyQueueCounts[NUM_SPECIALTIES] = {0};
+int patientCount = 0;
+char patientNames[MAX_PATIENTS][50];
+int patientAges[MAX_PATIENTS];
+int patientUrgencyLevels[MAX_PATIENTS];
+int patientSpecialtyIDs[MAX_PATIENTS];
+int patientIsAdmitted[MAX_PATIENTS];
+int patientWardIDs[MAX_PATIENTS];
+int patientDaysAdmitted[MAX_PATIENTS];
+double patientFinalAmounts[MAX_PATIENTS];
